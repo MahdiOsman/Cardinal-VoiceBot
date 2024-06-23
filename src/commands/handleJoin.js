63a67bb4be77fc8dbf1ad728cc16d2ec9f3e5072
Evgenii Gulev <76736526+EvgeniiGulev@users.noreply.github.com>
2024-06-23 21:18:30 +0300

    Bot can now check for command mid sentence

--- src/commands/handleJoin.js
+++ src/commands/handleJoin.js
@@ -7,50 +7,54 @@ const recognizeSpeech = require("../utils/speechUtils");
 const handleVoiceCommand = require("../eventHandlers/voiceCommandHandler");
 
 module.exports = async (message) => {
-    const { channel } = message.member.voice;
-    if (!channel) {
-        return message.reply(
-            "You need to be in a voice channel to use this command."
-        );
-    }
+  const { channel } = message.member.voice;
+  if (!channel) {
+    return message.reply(
+      "You need to be in a voice channel to use this command."
+    );
+  }
 
-    try {
-        const connection = joinVoiceChannel({
-            channelId: channel.id,
-            guildId: message.guild.id,
-            adapterCreator: message.guild.voiceAdapterCreator,
-            selfDeaf: false,
-        });
+  try {
+    const connection = joinVoiceChannel({
+      channelId: channel.id,
+      guildId: message.guild.id,
+      adapterCreator: message.guild.voiceAdapterCreator,
+      selfDeaf: false,
+    });
 
-        connection.receiver.speaking.on("start", (userId) => {
-            //console.log(`Started recording for user ${userId}`);
-            startRecording(userId, connection.receiver);
-        });
+    connection.receiver.speaking.on("start", (userId) => {
+      //console.log(`Started recording for user ${userId}`);
+      startRecording(userId, connection.receiver);
+    });
 
-        connection.receiver.speaking.on("end", (userId) => {
-            stopRecording(userId);
-            //console.log(`Stopped recording for user ${userId}`);
+    connection.receiver.speaking.on("end", (userId) => {
+      stopRecording(userId);
+      //console.log(`Stopped recording for user ${userId}`);
 
-            recognizeSpeech(userId, (err, result) => {
-                if (err) {
-                    console.error("Error recognizing speech:", err);
-                    return;
-                }
+      recognizeSpeech(userId, (err, result) => {
+        if (err) {
+          console.error("Error recognizing speech:", err);
+          return;
+        }
 
-                const recognizedText = result.toLowerCase();
-                recognizedText.replace(",", "");
-                // Check if the recognized text starts with "cardinal"
-                if (recognizedText.startsWith("cardinal")) {
-                    const command = recognizedText.split(" ")[1];
-                    const prompt = recognizedText.split(" ").slice(2).join(" ");
-                    handleVoiceCommand(command, message, prompt);
-                }
-            });
-        });
+        const recognizedText = result.toLowerCase().replace(/[^a-z0-9 ]/g, "");
+        const activationWordIndex = recognizedText
+          .split(" ")
+          .indexOf("cardinal");
+        if (activationWordIndex != -1) {
+          const command = recognizedText.split(" ")[activationWordIndex + 1];
+          const prompt = recognizedText
+            .split(" ")
+            .slice(activationWordIndex + 2)
+            .join(" ");
+          handleVoiceCommand(command, message, prompt);
+        }
+      });
+    });
 
-        await message.reply(`Joined ${channel.name}!`);
-    } catch (error) {
-        console.error(error);
-        await message.reply("There was an error joining the voice channel.");
-    }
+    await message.reply(`Joined ${channel.name}!`);
+  } catch (error) {
+    console.error(error);
+    await message.reply("There was an error joining the voice channel.");
+  }
 };
--- src/eventHandlers/voiceCommandHandler.js
+++ src/eventHandlers/voiceCommandHandler.js
@@ -1,48 +1,49 @@
 const {
-    handleLeaveCommand,
-    handleSkipCommand,
-    handlePauseCommand,
-    handleUnpauseCommand,
-    playAudioFromYouTube,
-    pauseAudio,
-    unpauseAudio,
-    handleMuteCommand,
-    handleSearchCommand,
+  handleLeaveCommand,
+  handleSkipCommand,
+  handlePauseCommand,
+  handleUnpauseCommand,
+  playAudioFromYouTube,
+  pauseAudio,
+  unpauseAudio,
+  handleMuteCommand,
+  handleSearchCommand,
 } = require("../commands");
 
 module.exports = (command, message, prompt) => {
-    const commandHandlers = {
-        // Handle Leave Commands
-        leave: handleLeaveCommand,
-        disconnect: handleLeaveCommand,
-        disappear: handleLeaveCommand,
-        vanish: handleLeaveCommand,
-        die: handleLeaveCommand,
+  const commandHandlers = {
+    // Handle Leave Commands
+    leave: handleLeaveCommand,
+    disconnect: handleLeaveCommand,
+    disappear: handleLeaveCommand,
+    vanish: handleLeaveCommand,
+    die: handleLeaveCommand,
 
-        // Handle Music Commands
-        skip: handleSkipCommand,
-        next: handleSkipCommand,
-        pause: pauseAudio,
-        unpause: unpauseAudio,
-        play: playAudioFromYouTube,
-        mute: handleMuteCommand,
-        stop: handleMuteCommand,
+    // Handle Music Commands
+    skip: handleSkipCommand,
+    next: handleSkipCommand,
+    pause: pauseAudio,
+    unpause: unpauseAudio,
+    resume: unpauseAudio,
+    play: playAudioFromYouTube,
+    mute: handleMuteCommand,
+    stop: handleMuteCommand,
 
-        // Handle OpenAI Commands
-        search: handleSearchCommand,
-    };
+    // Handle OpenAI Commands
+    search: handleSearchCommand,
+  };
 
-    // Clean up the message (remove caps/extra spaces/special characters)
+  // Clean up the message (remove caps/extra spaces/special characters)
 
-    command = command.toLowerCase().replace(/[^a-z0-9 ]/g, "");
-    console.log(prompt);
-    // If the command is recognized, call the appropriate handler
-    const handler = commandHandlers[command];
-    if (handler) {
-        handler(message, prompt);
-    } else {
-        console.error(`Command ${command} unknown.`);
-    }
+  command = command.toLowerCase().replace(/[^a-z0-9 ]/g, "");
+  console.log(prompt);
+  // If the command is recognized, call the appropriate handler
+  const handler = commandHandlers[command];
+  if (handler) {
+    handler(message, prompt);
+  } else {
+    console.error(`Command ${command} unknown.`);
+  }
 };
 
 /* message.content.split(" ").slice(2).join(" "); */
